cc9c969b0e52d367d52625ae54c102ecddded621
Kolya <koliachipurko@gmail.com>
2023-10-24 21:14:43 +0300

    Дз2

---
+++ main.py
@@ -0,0 +1,16 @@
+shopping_list =[]
+shopping_list.append("яблоко")
+shopping_list.append("молоко")
+shopping_list.append("хлеб")
+shopping_list.append("яйца")
+shopping_list.append("соль")
+print (shopping_list)
+for item in shopping_list:
+    print(item)
+
+del shopping_list [1]
+print (shopping_list)
+shopping_list[0] = "банан"
+print (shopping_list)
+b =len(shopping_list)
+print (b)
