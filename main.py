ae8c22623de31b11b5c69b94a3935af9c911b6b0
Kolya <koliachipurko@gmail.com>
2023-11-18 16:00:43 +0300

    ddddddddzzzzzz

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-'''shopping_list =[]
-shopping_list.append("яблоко")
-shopping_list.append("молоко")
-shopping_list.append("хлеб")
-shopping_list.append("яйца")
-shopping_list.append("соль")
-print (shopping_list)
-for item in shopping_list:
-    print(item)
-
-del shopping_list [1]
-print (shopping_list)
-shopping_list[0] = "банан"
-print (shopping_list)
-b =len(shopping_list)
-print (b)'''
-'''N= int(input())
-k = int(input())-1
-people = list(range(1,N+1))
-while len(people) !=1:
-    k = k% len(people)
-    del people[k]
-    k+=2
-    print (people)'''
 s = input("Введите текст:")
 a = 0
 vowels = set("аеияо")
