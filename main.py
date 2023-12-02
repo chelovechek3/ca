6a6f48bddd4f3a4a201abfcac6c6d8a27615c5fa
Kolya <koliachipurko@gmail.com>
2023-12-02 23:32:48 +0300

    func

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-s = input("Введите текст:")
+"""s = input("Введите текст:")
 a = 0
 vowels = set("аеияо")
 for letter in s:
     if letter in vowels:
         a += 1
 print("Количество гласных:")
-print(a)
+print(a)"""
+d =  int(input("Введите первое число: "))
+x= input("Введите оператор действия: ")
+f =  int(input("Введите второе число: "))
+def myfunc ():
+ print(d + f)
+def myfunc1 ():
+ print(d-f)
+def myfunc2():
+ print(d*f)
+def myfunc3():
+    print(d**f)
+def myfunc4():
+    print(d//f)
+def myfunc5():
+ print(d%f)
+def myfunc6():
+    print(d/f)
+if x=="+":
+     myfunc()
+elif x=="-":
+    myfunc1()
+elif x=="*":
+    myfunc2()
+elif x=="**":
+ myfunc3()
+elif x=="//" and f!=0:
+    myfunc4()
+elif x =="%" and f!=0:
+    myfunc5()
+elif x =="/" and f!=0:
+    myfunc6()
+else: print("Вы совершили ошибку в введении данных")
+
+
+
+
 
 
 
